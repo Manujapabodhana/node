060a41bc8f119c93b15074a99e706f0126d1d00d
manuja <rpmanuja123@gmail.com>
2025-05-29 12:30:44 +0530

    hh

--- yt-tutorial/index.js
+++ yt-tutorial/index.js
@@ -1,3 +1,10 @@
-const os= require ("os");
+import os from "os";
+import amila from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = amila.dirname(__filename);
 
 console.log(os.platform());
+
+
+console.log(amila.basename(--filename)); 
