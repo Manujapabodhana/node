f1d7ca815e59d6f676ce8c33bd53ed6fe4499f2d
manuja <rpmanuja123@gmail.com>
2025-05-31 07:53:57 +0530

    Update myfuntion.js

--- yt-tutorial/myfuntion.js
+++ yt-tutorial/myfuntion.js
@@ -2,7 +2,7 @@ const math=(number1, number2,number3,number4) => {
     let a= number1 + number2*2;
     if(number3){
         a += number3 -5;
-        }else if(number4){
+        } if(number4){
             a *= number4;
         }
     return a;
